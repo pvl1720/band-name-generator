f63e6c941facbb6e5445b1510f420b61a9d69553
Maksim Mikoss <pvl1720@gmail.com>
2015-06-16 19:11:05 -0700

    q

---
+++ server.js
@@ -0,0 +1,34 @@
+'use strict'
+
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/app/'));
+
+var Adjective = function() {
+ this.sleepy = true;
+ this.fuzzy = true;
+ this.cranky = true;
+ this.soporific = true;
+ this.eloquent = true;
+};
+
+var adjective = new Adjective();
+
+function getRandomWord (object) {
+ var propArray = Object.keys(object);
+ var randomProp = propArray[Math.floor(Math.random() * propArray.length)];
+ return {word: randomProp};}
+
+app.get('/', function(req, res) {
+ res.send('thug life!');
+});
+
+app.get('/adjective', function(req, res) {
+ res.json(getRandomWord(adjective));
+});
+
+app.listen(port, function() {
+ console.log('server available at http://localhost: ' + port);
+});
